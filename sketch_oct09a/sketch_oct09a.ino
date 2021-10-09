ba54355507925b8603a4b2cc7d8422963a8955b1
ferdin <ferdinbruh1234@gmail.com>
2021-10-09 18:12:29 +0200

    1 prototyp

---
+++ sketch_oct09a/sketch_oct09a.ino
@@ -0,0 +1,39 @@
+int x;
+boolean eins;
+
+void setup()
+{
+pinMode(13, OUTPUT);
+pinMode(2, INPUT);
+boolean inputRead;
+}
+
+void loop() {
+  
+  boolean vorRead = digitalRead(2);
+  if (vorRead==true)
+  {
+    eins = true;
+  }
+  
+  if(!vorRead && eins == true)
+  {
+    x += 1;
+   eins = false;   
+  }
+
+  if(x>=2)
+  {
+  for(int i=0; i < 3; i = i+1)
+  {
+  digitalWrite(13, HIGH);
+  delay(1000);
+  
+  digitalWrite(13, LOW);
+  delay(1000);
+  }
+  
+  x = 0;
+  
+  }
+}
