ce0a1e0abee8d7d4d50d542b8d648223a81e9a9d
Furko123 <h.furkan.oezkan@gmail.com>
2021-10-18 15:24:52 +0200

    Cleaning up code and renaming variables

--- sketch_oct09a/sketch_oct09a.ino
+++ sketch_oct09a/sketch_oct09a.ino
@@ -1,12 +1,9 @@
-int x;
+int corIn; // Number of inputs that correspond to the selected pattern
 boolean eins;
-boolean zwei;
-boolean drei;
-boolean vier;
-boolean fuenf;
+boolean reco;
 boolean recordboolean;
 int drueckanzahl = 2;
-int zeit = 2;
+int timeInterval = 2;
 int starttime = 0;
 int endtime = 0;
 int elapsedtime = 0;
@@ -24,11 +21,6 @@ void setup()
 
 void loop()
 {
-  vorReadRecord = digitalRead(3);
-
-  if (vorReadRecord == true)
-    zwei = true;
-
   if (recordboolean)
     record();
   else
@@ -37,22 +29,27 @@ void loop()
 
 void record()
 {
-  if (!vorReadRecord && zwei)
+  if (!vorReadRecord && reco)
   {
     recordboolean = false;
-    zwei = false;
+    reco = false;
   }
 }
 
 void detectPattern()
 {
+  vorReadRecord = digitalRead(3);
+
+  if (vorReadRecord == true)
+    reco = true;
+    
   if (getVorRead() == false)
     eins = true;
     
-  if (!vorReadRecord && zwei)
+  if (!vorReadRecord && reco)
   {
     recordboolean = true;
-    zwei = false;
+    reco = false;
   }
 
   if (getVorRead() && eins == true)
@@ -61,18 +58,18 @@ void detectPattern()
     elapsedtime = endtime - starttime; // Wie viel zeit seit dem letzten drücken vergangen ist
   }
 
-  if (getVorRead() && eins == true && ((elapsedtime >= 500 * zeit && elapsedtime <= 1000 * zeit) || elapsedtime == endtime))
+  if (getVorRead() && eins == true && ((elapsedtime >= 500 * timeInterval && elapsedtime <= 1000 * timeInterval) || elapsedtime == endtime))
   {
-    x += 1;
+    corIn += 1;
     eins = false;
     starttime = millis();
   }
-  else if (getVorRead() && eins == true && !((elapsedtime >= 500 * zeit && elapsedtime <= 1000 * zeit) || elapsedtime == endtime))
+  else if (getVorRead() && eins == true && !((elapsedtime >= 500 * timeInterval && elapsedtime <= 1000 * timeInterval) || elapsedtime == endtime))
   {
     starttime = 0;
-    x = 0;
+    corIn = 0;
   }
-  if (x >= drueckanzahl)
+  if (corIn >= drueckanzahl)
   {
     for (int i = 0; i < 3; i = i + 1)
     {
@@ -83,7 +80,7 @@ void detectPattern()
       delay(1000);
       starttime = 0;
     }
-    x = 0;
+    corIn = 0;
   }
 }
 
@@ -92,8 +89,8 @@ float getElectrodeValue()
   float sensorValue = analogRead(A0);
   float millivolt = (sensorValue / 1023) * 5;
   Serial.print(millivolt);
-  Serial.print ("x: ");
-  Serial.print(x);
+  Serial.print ("Correct input: ");
+  Serial.print(corIn);
   Serial.println();
   delay(100);
   return millivolt;
